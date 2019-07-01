07613f6b9a0662c19270074ce44d9174fec3226e
Sonia Gama de la Cruz <abinyuuu@gmail.com>
2019-07-01 04:25:13 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,12 +7,9 @@ def summon_captain_planet(array)
 end
 
 def long_planeteer_calls(array)
-  array.each do |word|
-    
-  end
-  
-  words.any? do |word|
+  array.any? do |word|
   word.length > 4
+  
   end
 end
 
